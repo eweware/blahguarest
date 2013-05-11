ca981169a693190d6dc77a344299654a90dd7adc
Roo deKoo <rk_java@yahoo.com>
2013-05-11 16:46:08 -0700

    https://eweware.atlassian.net/browse/WRS-311

--- src/main/java/com/eweware/service/rest/resource/UsersResource.java
+++ src/main/java/com/eweware/service/rest/resource/UsersResource.java
@@ -147,7 +147,6 @@ public class UsersResource {
     @POST
     @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Response loginUser(UserPayload entity,
                               @Context HttpServletRequest request) {
         try {
