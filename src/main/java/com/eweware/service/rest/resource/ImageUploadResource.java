60febf74e776cb4f43b6437a5539f6b0c7c0f353
Roo deKoo <rk_java@yahoo.com>
2013-06-18 20:59:21 -0700

    BWC-1226. Pointing to PROD for deployment

--- src/main/java/com/eweware/service/rest/resource/ImageUploadResource.java
+++ src/main/java/com/eweware/service/rest/resource/ImageUploadResource.java
@@ -180,7 +180,6 @@ public class ImageUploadResource {
     }
 
     private Response doUpload(String objectType, String objectId, InputStream in, FormDataContentDisposition metadata, HttpServletRequest request) {
-        logger.info("objectType="+objectType);
         final MediaReferendType referendType;
         try {
             BlahguaSession.ensureAuthenticated(request);
